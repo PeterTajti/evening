6758c9f8b453219fd4c17dd5294c469ce0c7e717
PeterTajti <peter.tajti@gmail.com>
2022-07-01 22:53:50 +0200

    boolean method initial

---
+++ src/com/gfa/exam/SymmetricMatrix.java
@@ -0,0 +1,14 @@
+package com.gfa.exam;
+
+import java.util.ArrayList;
+
+public class SymmetricMatrix {
+    public static void main(String[] args) {
+
+        //isSymmetric();
+    }
+
+    public static boolean isSymmetric(int[][] matrix) {
+        return true;
+    }
+}
